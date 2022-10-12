789b2ac3ba43d2d96d5f20f870bc03c39cba3672
fabiolasiles <fabiola.siles93@gmail.com>
2022-10-11 23:32:09 -0300

    se subio el tp1 terminado y el tp2 a realizar

---
+++ TP2/increidle.h
@@ -0,0 +1,11 @@
+#ifndef __INCREIDLE_H__
+#define __INCREIDLE_H__
+
+
+
+
+
+
+
+
+#endif /* __INCREIDLE_H__ */
---
+++ TP2/juego.c
@@ -0,0 +1,13 @@
+#include "kronos.h"
+#include "increidle.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(){
+
+    
+}

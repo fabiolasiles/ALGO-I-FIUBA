f7e9ccd6dc2926e144decac27f9493601ea0a91b
fabiola-siles <fsiles@fi.uba.ar>
2022-11-05 02:17:36 -0300

    prueba coordenadas alrededor del robot hecha

--- TP2_mejorado/juego.c
+++ TP2_mejorado/juego.c
@@ -10,7 +10,16 @@
 
 int main(){
 
-    inicializar();
+    //inicializar();
+    coordenada_t posicionDelRobot;
+    posicionDelRobot.fila = 5;
+    posicionDelRobot.columna= 5;
+    coordenada_t posicionesVecinas[MAX_COORDENADAS];
+    int tope = 8;
+    asignarCoordenadasVecinasAlRobot(posicionDelRobot,posicionesVecinas);
+    for(int i=0;i<tope;i++){
+        mostrarCoordenada(posicionesVecinas[i]);
+    }
     return 0;
 
 }
